7a85b7de25fc587f1b4fd3e9ae63ce1c3710455f
Kozlak <kozjohn@dunwoody.edu>
2018-11-02 10:53:06 -0500

    js

---
+++ mnRest.js
@@ -0,0 +1,690 @@
+
+
+var data = [
+   {
+     "Name": "Ruth's Chris Steak House",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "Manny's Steakhouse",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "Fogo de Chao Brazilian Steakhouse",
+     "Type": "brazilian",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "The Capital Grille",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "",
+     "Type": "",
+     "Location": "downtown",
+     "Bar": "",
+     "Brunch": ""
+   },
+   {
+     "Name": "The Capital Grille",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": ""
+   },
+   {
+     "Name": "Keys Cafe",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "no",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "Sanctuary",
+     "Type": "asian",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "no"
+   },
+   {
+     "Name": "Hell's Kitchen",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "Dakota Jazz Club & Restaurant",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "no"
+   },
+   {
+     "Name": "Kieran's Irish Pub",
+     "Type": "irish",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "Sea Change",
+     "Type": "seafood",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "no"
+   },
+   {
+     "Name": "BANK Restaurant",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "Crave",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "Kindee Thai Restaurant",
+     "Type": "asian",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": ""
+   },
+   {
+     "Name": "McCormick & Schmick's Seafood & Steaks",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "8th Street Grill & Taphouse",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "Shout House Dueling Pianos",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "no"
+   },
+   {
+     "Name": "Normandy Kitchen",
+     "Type": "american",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "yes"
+   },
+   {
+     "Name": "O'Donovan's Irish Pub",
+     "Type": "irish",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "no"
+   },
+   {
+     "Name": "Dan Kelly's Bar and Grill",
+     "Type": "irish",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "no"
+   },
+   {
+     "Name": "Andrea Pizza",
+     "Type": "italian",
+     "Location": "downtown",
+     "Bar": "no",
+     "Brunch": "no"
+   },
+   {
+     "Name": "Wasabi",
+     "Type": "asian",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "no"
+   },
+   {
+     "Name": "Dancing Ganesha",
+     "Type": "indian",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "no"
+   },
+   {
+     "Name": "Seven Steakhouse Sushi & Rooftop",
+     "Type": "asian",
+     "Location": "downtown",
+     "Bar": "yes",
+     "Brunch": "no"
+   },
+   {
+     "Name": "Saint Dinette",
+     "Type": "American",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "Yes"
+   },
+   {
+     "Name": "Bulldog",
+     "Type": "American",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Handsome Hog",
+     "Type": "Southern",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "Yes"
+   },
+   {
+     "Name": "Dark Horse",
+     "Type": "Southern",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Kyatchi",
+     "Type": "Japanese",
+     "Location": "Lowertown",
+     "Bar": "No",
+     "Brunch": "No"
+   },
+   {
+     "Name": "PUBLIC kitchen + bar",
+     "Type": "American",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Mucci",
+     "Type": "Italian",
+     "Location": "St Paul",
+     "Bar": "Yes",
+     "Brunch": "Yes"
+   },
+   {
+     "Name": "Octo Bar",
+     "Type": "Seafood",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "Yes"
+   },
+   {
+     "Name": "Oxcart Ale House",
+     "Type": "American",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Kelly Depot",
+     "Type": "Bar",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Barrio",
+     "Type": "Spanish",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Big River Pizza",
+     "Type": "Italian",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "World of Beer",
+     "Type": "Bar",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Asian Express",
+     "Type": "Chinese",
+     "Location": "Lowertown",
+     "Bar": "No",
+     "Brunch": "Yes"
+   },
+   {
+     "Name": "Meritage",
+     "Type": "French",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "Yes"
+   },
+   {
+     "Name": "Gray Duck Tavern",
+     "Type": "Bar",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "The St Paul Grill",
+     "Type": "",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "Yes"
+   },
+   {
+     "Name": "Union Bar and Grill",
+     "Type": "American",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Black Sheep",
+     "Type": "Italian",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Pino Pizzeria",
+     "Type": "Italian",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Rival House",
+     "Type": "American",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Herbies on the Park",
+     "Type": "American",
+     "Location": "Lowertown",
+     "Bar": "Yes",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Cassie's Deli",
+     "Type": "Sandwhich",
+     "Location": "Lowertown",
+     "Bar": "No",
+     "Brunch": "No"
+   },
+   {
+     "Name": "Skyva Deli",
+     "Type": "Sandwhich",
+     "Location": "Lowertown",
+     "Bar": "No",
+     "Brunch": "No"
+   },
+      {
+        "name": "Lake irving Restaurant and Bar",
+        "type": "American",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "Yes"
+      },
+      {
+        "name": "Rinata",
+        "type": "Italian",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "Barbette",
+        "type": "French",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "Yes"
+      },
+      {
+        "name": "Kihndo Restaurant",
+        "type": "Asian",
+        "location": "Uptown, MN",
+        "bar": "No",
+        "brunch": "No"
+      },
+      {
+        "name": "Origami Restaurant",
+        "type": "Japanese",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "Chino Latino",
+        "type": "Fusion",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "Amore Uptown",
+        "type": "Italian",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "The Lowry",
+        "type": "American",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "India Palace Uptown",
+        "type": "Indian",
+        "location": "Uptown, MN",
+        "bar": "No",
+        "brunch": "No"
+      },
+      {
+        "name": "Red Cow Uptown",
+        "type": "Gastropub",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "Yes"
+      },
+      {
+        "name": "Namaste Caf%u017D",
+        "type": "Nepalese",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "Agra Culture Kitchen & Press",
+        "type": "Health Food",
+        "location": "Uptown, MN",
+        "bar": "No",
+        "brunch": "No"
+      },
+      {
+        "name": "Amazing Thailand",
+        "type": "Thai",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "The Lotus Uptown",
+        "type": "Vietnamese",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "Kiku Bistro",
+        "type": "Japanese",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "Uptown Diner",
+        "type": "American",
+        "location": "Uptown, MN",
+        "bar": "No",
+        "brunch": "No"
+      },
+      {
+        "name": "Soho Caf%u017D",
+        "type": "Italian",
+        "location": "Uptown, MN",
+        "bar": "No",
+        "brunch": "No"
+      },
+      {
+        "name": "Darbar India Bar & Grill",
+        "type": "Indian",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "French Meadow Bakery & Caf%u017D",
+        "type": "American",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "Yes"
+      },
+      {
+        "name": "Giordano's",
+        "type": "Italian",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "Hoban Korean BBQ",
+        "type": "Korean",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "Mesa Pizza Uptown",
+        "type": "Italian",
+        "location": "Uptown, MN",
+        "bar": "No",
+        "brunch": "No"
+      },
+      {
+        "name": "Roat Osha",
+        "type": "Thai",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+      {
+        "name": "Burch Steak",
+        "type": "American",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "Yes"
+      },
+      {
+        "name": "Pizza Luc%u017D Uptown",
+        "type": "Italian",
+        "location": "Uptown, MN",
+        "bar": "Yes",
+        "brunch": "No"
+      },
+         {
+           "Name": "El Taco Riendo",
+           "Type": "Mexican",
+           "Location": "Northeast Minneapolis",
+           "Bar": "No",
+           "Brunch": "No"
+         },
+         {
+           "Name": "Maya Cuisine",
+           "Type": "Mexican",
+           "Location": "Northeast Minneapolis",
+           "Bar": "Yes",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "Holy Land",
+           "Type": "Mediterranean",
+           "Location": "Northeast Minneapolis",
+           "Bar": "No",
+           "Brunch": "No"
+         },
+         {
+           "Name": "The Mill",
+           "Type": "American",
+           "Location": "Northeast Minneapolis",
+           "Bar": "Yes",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "Masu Sushi",
+           "Type": "Japanese",
+           "Location": "St. Anthony Main",
+           "Bar": "Yes",
+           "Brunch": "No"
+         },
+         {
+           "Name": "The Sheridan Room",
+           "Type": "American",
+           "Location": "Northeast Minneapolis",
+           "Bar": "Yes",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "Fat Nat's Eggs",
+           "Type": "American",
+           "Location": "St. Anthony Village",
+           "Bar": "No",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "Brasa",
+           "Type": "Mexican",
+           "Location": "St. Anthony Main",
+           "Bar": "No",
+           "Brunch": "No"
+         },
+         {
+           "Name": "Young Joni",
+           "Type": "Korean",
+           "Location": "Northeast Minneapolis",
+           "Bar": "Yes",
+           "Brunch": "No"
+         },
+         {
+           "Name": "Revival",
+           "Type": "American",
+           "Location": "Southwest Minneapolis",
+           "Bar": "Yes",
+           "Brunch": "No"
+         },
+         {
+           "Name": "Northeast Social",
+           "Type": "American",
+           "Location": "Northeast Minneapolis",
+           "Bar": "Yes",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "The Draft House",
+           "Type": "American",
+           "Location": "Northeast Minneapolis",
+           "Bar": "Yes",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "The Anchor Fish & Chips",
+           "Type": "American",
+           "Location": "Northeast Minneapolis",
+           "Bar": "Yes",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "Hazel's Northeast",
+           "Type": "American",
+           "Location": "Northeast Minneapolis",
+           "Bar": "No",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "Erte",
+           "Type": "French",
+           "Location": "Northeast Minneapolis",
+           "Bar": "No",
+           "Brunch": "No"
+         },
+         {
+           "Name": "Costa Blanca Bistro",
+           "Type": "Spanish",
+           "Location": "Northeast Minneapolis",
+           "Bar": "Yes",
+           "Brunch": "No"
+         },
+         {
+           "Name": "PinkU",
+           "Type": "Japanese",
+           "Location": "St. Anthony West",
+           "Bar": "No",
+           "Brunch": "No"
+         },
+         {
+           "Name": "Red Stag",
+           "Type": "American",
+           "Location": "St. Anthony East",
+           "Bar": "Yes",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "Chimborazo",
+           "Type": "Ecuadorian",
+           "Location": "Northeast Minneapolis",
+           "Bar": "No",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "Que Viet",
+           "Type": "Vietnamese",
+           "Location": "Northeast Minneapolis",
+           "Bar": "No",
+           "Brunch": "No"
+         },
+         {
+           "Name": "Hai Hai",
+           "Type": "Vietnamese",
+           "Location": "NE Minneapolis Arts District",
+           "Bar": "No",
+           "Brunch": "No"
+         },
+         {
+           "Name": "Jax Caf�",
+           "Type": "American",
+           "Location": "NE Minneapolis Arts District",
+           "Bar": "Yes",
+           "Brunch": "Yes"
+         },
+         {
+           "Name": "Uncle Franky's",
+           "Type": "American",
+           "Location": "St. Anthony East",
+           "Bar": "No",
+           "Brunch": "No"
+         },
+         {
+           "Name": "Element Wood Fire Pizza",
+           "Type": "Italian",
+           "Location": "Northeast Minneapolis",
+           "Bar": "Yes",
+           "Brunch": "No"
+         }
+       ]
